021bb4899aee3ad47e091ef5059a66f90166b145
Hassannewcode <privatealkassan@gmail.com>
2025-08-03 11:07:48 +0300

    Add files via upload

---
+++ root-directory/PaperclipIcon.tsx
@@ -0,0 +1,8 @@
+
+import React from 'react';
+
+export const PaperclipIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.75" stroke="currentColor" {...props}>
+    <path strokeLinecap="round" strokeLinejoin="round" d="M18.375 12.739l-7.693 7.693a4.5 4.5 0 01-6.364-6.364l10.94-10.94A3.375 3.375 0 0119.5 7.372a3.375 3.375 0 01-3.375 3.375l-10.94 10.94a1.5 1.5 0 01-2.122-2.122l7.693-7.693-1.622-1.622-7.693 7.693a4.5 4.5 0 006.364 6.364l10.94-10.94A3.375 3.375 0 0016.125 4.628a3.375 3.375 0 00-3.375 3.375l-10.94 10.94a1.5 1.5 0 002.122 2.122l7.693-7.693 1.622 1.622z" />
+  </svg>
+);
---
+++ root-directory/RefreshIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const RefreshIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.75" stroke="currentColor" {...props}>
+        <path strokeLinecap="round" strokeLinejoin="round" d="M19.933 13.041a8 8 0 1 1-9.925-8.788c3.899-1 7.935 1.007 9.425 4.747" />
+        <path strokeLinecap="round" strokeLinejoin="round" d="M20 4v5h-5" />
+    </svg>
+);
