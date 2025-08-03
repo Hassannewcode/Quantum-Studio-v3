fda780bb38d033a8f12c4bad5874e30b4bc4d9b8
Hassannewcode <privatealkassan@gmail.com>
2025-08-03 11:04:29 +0300

    Delete Quantum directory

--- Quantum/components/icons/RectangleIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const RectangleIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.75" stroke="currentColor" {...props}>
-        <path strokeLinecap="round" strokeLinejoin="round" d="M21 7.5l-2.25-1.313M21 7.5v9M21 7.5l-9 5.25M3 7.5l2.25-1.313M3 7.5v9M3 7.5l9 5.25m9-9l-9 5.25m0 0l-9-5.25m9 5.25v9l-9-5.25M12 21l9-5.25" opacity="0.25" />
-        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 4.5h16.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25H3.75a2.25 2.25 0 01-2.25-2.25V6.75A2.25 2.25 0 013.75 4.5z" />
-    </svg>
-);
--- Quantum/components/icons/TypographyIcon.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const TypographyIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.75" stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h.008v.008H12v-.008z" />
-    <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 6.75V17.25M17.25 6.75V17.25" />
-  </svg>
-);
